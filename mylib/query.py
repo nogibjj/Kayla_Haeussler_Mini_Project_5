1fa57e40d4058f3eeeeeea2b18712b5862581879
Kayla Haeusssler <kaylahaeusssler@Kaylas-MBP.lan>
2024-10-06 16:09:30 -0400

    initial commit

--- mylib/query.py
+++ mylib/query.py
@@ -3,14 +3,52 @@
 import sqlite3
 
 
-def query():
-    """Query the database for the top 5 rows of the GroceryDB table"""
-    conn = sqlite3.connect("GroceryDB.db")
+def create():
+    """Create a fake data"""
+    conn = sqlite3.connect("Candy_DB.db")
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM GroceryDB")
-    print("Top 5 rows of the GroceryDB table:")
+    cursor.execute(
+        "INSERT INTO Candy_DB VALUES ('Data Engineering','0','0','0','0', '0','0','0','0', '0','0','0','0')"
+    )
+    conn.commit()
+    conn.close()
+    return "Sucessfully created!"
+
+
+def read():
+    """Read and print the database for all the rows of the candy table"""
+    conn = sqlite3.connect("Candy_DB.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Candy_DB")
     print(cursor.fetchall())
     conn.close()
-    return "Success"
+    return "Successfully read!"
 
 
+def update():
+    """Update day of week value of 1 and set the births to 1000"""
+    conn = sqlite3.connect("Candy_DB.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        "UPDATE Candy_DB SET competitorname = 'LOSER' WHERE winpercent < '50';"
+    )
+    conn.commit()
+    conn.close()
+    return "Successfully updated!"
+
+
+def delete():
+    """Delete all rows where candy contains chocolate"""
+    conn = sqlite3.connect("Candy_DB.db")
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM Candy_DB WHERE chocolate = '1';")
+    conn.commit()
+    conn.close()
+    return "Sucessfully deleted!"
+
+
+if __name__ == "__main__":
+    create()
+    read()
+    update()
+    delete()
--- test_main.py
+++ test_main.py
@@ -3,8 +3,39 @@ Test goes here
 
 """
 
-from mylib.calculator import add
+from mylib.extract import extract
+from mylib.transform_load import load
+from mylib.query import create, read, update, delete
 
 
-def test_add():
-    assert add(1, 2) == 3
+def test_extract():
+    assert extract() == "data/candy-data.csv"
+
+
+def test_load():
+    assert load() == "candy-data.db"
+
+
+def test_create():
+    assert create() == "Sucessfully created!"
+
+
+def test_read():
+    assert read() == "Successfully read!"
+
+
+def test_update():
+    assert update() == "Successfully updated!"
+
+
+def test_delete():
+    assert delete() == "Sucessfully deleted!"
+
+
+if __name__ == "__main__":
+    test_extract()
+    test_load()
+    test_create()
+    test_read()
+    test_update()
+    test_delete()
